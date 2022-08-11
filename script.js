ec30f7eac6a549bf279b442afd1183e2e5d89a95
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-11 12:15:48 +0200

    Learning about Destructuring objects

--- script.js
+++ script.js
@@ -28,238 +28,29 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  // ES6 enhanced object literals
-  hours,
-
-  order(starterIndex, mainIndex) {
+  order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-
-  orderPasta(ing1, ing2, ing3) {
-    console.log(
-      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
-
-  orderPizza(mainIngredient, ...otherIngredient) {
-    console.log(`${mainIngredient}`);
-    console.log(`${otherIngredient}`);
-  },
 };
-
-// LOOPING OBJECTS: OBJECT KEYS VALUES AND ENTRIES
-// Property names
-// const properties = Object.keys(hours);
-// console.log(properties);
-
-// let openStr = `We are open on ${properties.length} days: `;
-
-// for (const day of properties) {
-//   openStr += `${day}, `;
-// }
-
-// console.log(openStr);
-
-// Property values
-// const values = Object.values(hours);
-// console.log(values);
-
-// Entire object
-// const entries = Object.entries(hours);
-// console.log(entries);
-
-// for (const [key, { open, close }] of entries) {
-//   console.log(`On ${key} we open at ${open} and close at ${close}`);
-// }
-
-// ================================
-// MAP Iteration
-const question = new Map([
-  ['question', 'What is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct'],
-  [false, 'Try again!'],
-]);
-
-console.log(question);
-
-// Convert object to map
-console.log(Object.entries(hours));
-const hoursMap = new Map(Object.entries(hours));
-console.log(hoursMap);
-
-// Quiz app
-console.log(question.get('question'));
-
-for (const [key, value] of question) {
-  if (typeof key === 'number') {
-    console.log(`Answer ${key}: ${value}`);
-  }
-}
-// const answer = Number(prompt('Your answer: '));
-const answer = 3;
-console.log(answer);
-
-console.log(question.get(question.get('correct') === answer));
-
-// Convert map to array
-console.log([...question]);
-// console.log(question.entries());
-console.log(...question.keys());
-console.log(...question.values());
-
-// ================================
-// MAP Fundamentals
-// const rest = new Map();
-// rest.set('name', 'Classico Italiano');
-// rest.set(1, 'Firenze, Italy');
-// rest.set(2, 'Lisbon, Portugal');
-// console.log(rest.set(2, 'Lisbon, Portugal'));
-
-// rest
-//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-//   .set('open', 11)
-//   .set('close', 23)
-//   .set(true, 'We are open :D')
-//   .set(false, 'We are closed :(');
-
-// console.log(rest.get('name'));
-// console.log(rest.get(true));
-// console.log(rest.get(1));
-
-// const time = 8;
-// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
-
-// console.log(rest.has('categories'));
-// rest.delete(2);
-// rest.clear();
-
-// const arr = [1, 2];
-// rest.set(arr, 'Test');
-// rest.set(document.querySelector('h1'), 'Heading');
-// console.log(rest);
-// console.log(rest.size);
-
-// console.log(rest.get(arr));
-
-// ================================
-// SETS
-// const ordersSet = new Set([
-//   'Pasta',
-//   'Pizza',
-//   'Pizza',
-//   'Risotto',
-//   'Pasta',
-//   'Pizza',
-// ]);
-// console.log(ordersSet);
-
-// console.log(new Set('Jonas'));
-
-// console.log(ordersSet.size);
-// console.log(ordersSet.has('Pizza'));
-// console.log(ordersSet.has('Bread'));
-// ordersSet.add('Garlic Bread');
-// ordersSet.add('Garlic Bread');
-// ordersSet.delete('Risotto');
-// // ordersSet.clear();
-// console.log(ordersSet);
-
-// for (const order of ordersSet) {
-//   console.log(order);
-// }
-
-// EXAMPLE
-// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-// const staffUnique = [...new Set(staff)];
-// console.log(staffUnique);
-// console.log(
-//   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
-// );
-
-// OPTIONAL CHAINING (?.)
-// if (restaurant.hours && restaurant.hours.mon) {
-//   console.log(restaurant.hours.mon.open);
-// }
-
-// WITH OPTIONAL CHAINING
-// console.log(restaurant.hours?.mon?.open);
-
-// EXAMPLE
-// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-// for (const day of days) {
-//   const open = restaurant.hours[day]?.open ?? 'closed';
-//   console.log(`On ${day}, we open at ${open}`);
-// }
-
-// Methods
-// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
-
-// Array
-// const users = [{ name: 'Douglas', email: 'hello@douglas.io' }];
-// console.log(users[0]?.name ?? 'User array empty');
-
-// restaurant.orderDelivery({
-//   time: '23:30',
-//   address: 'Via del Sole, 21',
-//   mainIndex: 2,
-//   starterIndex: 2,
-// });
-
-// restaurant.orderDelivery({
-//   address: 'Via del Sole, 21',
-//   starterIndex: 2,
-// });
-
 // DESTRUCTURING OBJECTS
-// const { name, openingHours, categories } = restaurant;
-// console.log(name, openingHours, categories);
 
-// const {
-//   name: restaurantName,
-//   openingHours: hours,
-//   categories: tags,
-// } = restaurant;
-// console.log(restaurantName, hours, tags);
+// ===================================
+// DESTRUCTURING ARRAYS
+// const numbers = [2, 3, 4];
 
-// Default values
-// const { menu = [], starterMenu: starters = [] } = restaurant;
-// console.log(menu, starters);
+// const [num1, num2, num3] = numbers;
+// console.log(num1, num2, num3);
+// console.log(numbers);
 
-// Mutating variables
-// let a = 111;
-// let b = 999;
-// const obj = { a: 23, b: 7, c: 14 };
-// ({ a, b } = obj);
-// console.log(a, b);
-
-// Nested Objects
-// const {
-//   fri: { open, close },
-// } = openingHours;
-// console.log(open, close);
-
-// DESTRUCTURING ARRAY
 // let [main, , secondary] = restaurant.categories;
 // console.log(main, secondary);
 
-// Switching variables
+// Switching variable
 // const temp = main;
 // main = secondary;
 // secondary = temp;
 // console.log(main, secondary);
 
-// Using Destructiring
 // [main, secondary] = [secondary, main];
 // console.log(main, secondary);
 
@@ -267,185 +58,13 @@ console.log(...question.values());
 // const [starter, mainCourse] = restaurant.order(2, 0);
 // console.log(starter, mainCourse);
 
+// Nested Destructuring
 // const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 // console.log(i, j);
-
-// Nested destructuring
-// const [i, , [j, k]] = nested;
-// console.log(i, j, k);
+// const [i, , [j, u]] = nested;
+// console.log(i, j, u);
 
 // Default values
 // const [p = 1, q = 1, r = 1] = [8, 9];
 // console.log(p, q, r);
-
-// SPREAD
-// const arr = [7, 8, 9];
-// const newArr = [1, 2, arr[0], arr[1], arr[2]];
-// console.log(newArr);
-
-// const brandNewArr = [1, 2, ...arr];
-// console.log(brandNewArr);
-
-// console.log(...newArr);
-
-// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-// console.log(newMenu);
-
-// Copy array
-// const mainMenuCopy = [...restaurant.mainMenu];
-// console.log(mainMenuCopy);
-
-// Join 2 arrays
-// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-// console.log(menu);
-
-// Iterables: arrays, strings, maps, sets, NOT objects
-
-// const str = 'Douglas';
-// const letters = [...str, ' ', 'L.'];
-// console.log(letters);
-// console.log(...str);
-
-// REAL WORLD EXAMPLE
-// const ingredients = [
-//   prompt("Let's make pasta! Ingredient 1? "),
-//   prompt("Let's make pasta! Ingredient 2? "),
-//   prompt("Let's make pasta! Ingredient 3? "),
-// ];
-// console.log(ingredients);
-
-// restaurant.orderPasta(...ingredients);
-
-// OBJECTS
-// const newRestaurant = { foundedIn: 1997, ...restaurant, founder: 'Douglas' };
-// console.log(newRestaurant);
-
-// const restaurantCopy = { ...restaurant };
-// restaurantCopy.name = 'Ristorante Roma';
-
-// console.log(restaurantCopy.name);
-// console.log(restaurant.name);
-
-// REST PATTERNS AND PARAMETERS
-// 1) Destructuring
-// Spread, because on RIGHT side of =
-// const arr = [1, 2, 3, ...[4, 5]];
-
-// REST, because on the LEFT side of =
-// const [a, b, ...others] = [1, 2, 3, 4, 5];
-// console.log(a, b, others);
-
-// const [pizza, , risotto, ...otherFood] = [
-//   ...restaurant.mainMenu,
-//   ...restaurant.starterMenu,
-// ];
-// console.log(pizza, risotto, otherFood);
-
-// Objects (REST)
-// const { sat, ...weekDays } = restaurant.openingHours;
-// console.log(weekDays);
-
-// // 2) Functions
-// const add = function (...numbers) {
-//   let total = 0;
-//   for (let i = 0; i < numbers.length; i++) {
-//     total += numbers[i];
-//     // console.log(numbers[i]);
-//   }
-//   console.log(total);
-// };
-// add(1, 2);
-// add(1, 2, 5);
-// add(1, 2, 4, 5);
-
-// const x = [23, 5, 7];
-// add(...x);
-
-// restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
-// restaurant.orderPizza('mushrooms');
-
-// SHORT CIRCUITING (&& and ||)
-// console.log('---------- OR ----------');
-// User ANY data type, return ANY datat type, short-circuiting
-// console.log(3 || 'Jonas');
-// console.log('' || 'Jonas');
-// console.log(true || 0);
-// console.log(undefined || null);
-
-// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
-
-// restaurant.numGuests = 23;
-// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-// console.log(guests1);
-
-// const guests2 = restaurant.numGuests || 10;
-// console.log(guests2);
-
-// console.log('---------- AND ----------');
-// console.log(0 && 'Douglas');
-// console.log(7 && 'Douglas');
-// console.log('Hello' && 23 && null && 'douglas');
-
-// if (restaurant.orderPizza) {
-//   restaurant.orderPizza('mushrooms', 'spinach');
-// }
-
-// restaurant.order && restaurant.orderPizza('mushrooms', 'spinach');
-
-// NULLISH COALESCING OPERATOR
-// restaurant.numGuests = 0;
-
-// const guests = restaurant.numGuests || 10;
-// console.log(guests);
-
-// Nullish: null and undefined (NOT 0 or "");
-// const guestCorrect = restaurant.numGuests ?? 10;
-// console.log(guestCorrect);
-
-// LOGICAL ASSIGNMENT OPERATORS
-
-// const rest1 = {
-//   name: 'Capri',
-//   numGuests: 0,
-// };
-
-// const rest2 = {
-//   name: 'La Piazza',
-//   owner: 'Giovanni Rossi',
-// };
-
-// LOGICAL OR ASSIGNMENT OPERATOR
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
-
-// NULLISH ASSIGNMENT OPERATOR
-
-// rest1.numGuests ??= 10;
-// rest2.numGuests ??= 10;
-
-// rest1.owner = rest1.owner && '<ANONYYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYYMOUS>';
-
-// LOGICAL AND ASSIGNMENT OPERATOR
-// rest1.owner &&= '<ANONYYMOUS>';
-// rest2.owner &&= '<ANONYYMOUS>';
-
-// console.log(rest1);
-// console.log(rest2);
-
-// FOR ...OF LOOP
-// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-
-// for (const item of menu) {
-//   console.log(item);
-// }
-
-// for (const item of menu.entries()) {
-//   console.log(item);
-// }
-
-// ENHANCED OBJECT LITERALS
